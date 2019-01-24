38a02959f3ecc3da68d3f819eae72c99f8bff8ee
Jack Gamboa <jgamboa929@gmail.com>
2019-01-24 09:03:10 -0600

    Prep to deploy.

--- sweets-server/server.js
+++ sweets-server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
 const path = require("path");
 
 const products = require("./routes/products");
@@ -34,15 +34,29 @@ app.use("/api/donations", donations);
 app.use("/api/checkout", checkout);
 app.use("/api/carts", carts);
 
-// Server static assests if in production
+// Serve static assests from the React app if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("sweets-client/sweets/build"));
+  app.use(
+    express.static(path.join(__dirname, "sweets-client/sweets/client/build"))
+  );
+  // app.use(express.static("sweets-client/sweets/build"));
+
+  // The "catchall" handler: for any request that doesn't
+  // match one above, send back React's index.html file.
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "sweets", "build", "index.html")); // load the React index.html file
+    res.sendFile(
+      path.join(__dirname + "sweets-client/sweets/client/build/index.html")
+    ); // load the React index.html file
   });
+
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "sweets", "build", "index.html")); // load the React index.html file
+  // });
 }
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log("Server started on port ${PORT}", PORT);
